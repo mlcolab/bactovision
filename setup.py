ddc39d798e5591f99eba2939e7661fc354d04fef
StarostinV <vladimir.starostin@uni-tuebingen.de>
2024-02-12 16:18:49 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bactovision',
+    version='0.1.0',
+    description='A package for bacterial image processing',
+    author='Vladimir Starostin',
+    author_email='vladimir.starostin@uni-tuebingen.de',
+    url='https://github.com/StarostinV/bactovision',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'scikit-image',
+        'Pillow',
+        'opencv-python',
+        'anywidget',
+        'traitlets',
+        'matplotlib',
+        'jupyterlab',
+    ],
+)
